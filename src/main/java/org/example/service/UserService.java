e508842b6b20e0f518bda414470df4b81e2493cc
jekaBas <jekisbas@gmail.com>
2021-03-03 01:38:42 +0200

    complete UserService

--- src/main/java/org/example/service/UserService.java
+++ src/main/java/org/example/service/UserService.java
@@ -2,9 +2,19 @@ package org.example.service;
 
 import org.example.model.User;
 
+import java.util.List;
+
 public interface UserService {
 
     void saveUser(User user);
+
+    void updateUser(User user);
+
+    User getUserById(Long id);
+
+    List<User> getAllUsers();
+
+    void deleteUser(Long id);
 
     User findByUsername(String username);
 }
--- src/main/java/org/example/service/UserServiceImpl.java
+++ src/main/java/org/example/service/UserServiceImpl.java
@@ -4,12 +4,12 @@ import org.example.dao.UserDao;
 import org.example.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
-
-    private UserDao userDao;
+    private final UserDao userDao;
 
     @Autowired
     public UserServiceImpl(UserDao userDao) {
@@ -19,6 +19,26 @@ public class UserServiceImpl implements UserService {
     @Override
     public void saveUser(User user) {
         userDao.saveUser(user);
+    }
+
+    @Override
+    public void updateUser(User user) {
+        userDao.updateUser(user);
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return userDao.getUserById(id);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        userDao.deleteUser(id);
     }
 
     @Override
